b6c5ca90a947510625453b35ee3499df821ca3a7
Zhanyang Gong <98726175+AndyGongDS@users.noreply.github.com>
2022-07-26 22:07:42 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a Capstone Project for Springboard DS Fellowship',
+    author='Andy_Gong',
+    license='MIT',
+)
